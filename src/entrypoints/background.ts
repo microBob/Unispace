a23c4436546dbe6784382c8f33927cf87875bc76
Kenneth Yang <microbob@is88.com>
2024-12-18 10:52:39 -0800

    Figure out background

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,3 +1,7 @@
 export default defineBackground(() => {
-	console.log("Hello background!", { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  browser.tabs.onCreated.addListener((tab) => {
+    console.log("Tab created", tab);
+  });
 });
