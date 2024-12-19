f62d20d60cbdb68b6248b6cb0322f53dbceccd27
microBob <microBob@users.noreply.github.com>
2024-12-19 09:06:51 +0000

    Format code

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -2,17 +2,19 @@ import { init, id } from "@instantdb/core";
 import schema from "~/../instant.schema";
 
 export default defineBackground(() => {
-  console.log("Hello background!", { id: browser.runtime.id });
+	console.log("Hello background!", { id: browser.runtime.id });
 
-  // 1. Connect to InstantDB.
-  const db = init({
-    appId: import.meta.env.WXT_INSTANT_APP_ID,
-    schema,
-  });
+	// 1. Connect to InstantDB.
+	const db = init({
+		appId: import.meta.env.WXT_INSTANT_APP_ID,
+		schema,
+	});
 
-  console.log(db.transact(db.tx.workspace[id()].update({ name: "Test Workspace" })));
+	console.log(
+		db.transact(db.tx.workspace[id()].update({ name: "Test Workspace" })),
+	);
 
-  browser.tabs.onCreated.addListener((tab) => {
-    console.log("Tab created", tab);
-  });
+	browser.tabs.onCreated.addListener((tab) => {
+		console.log("Tab created", tab);
+	});
 });
