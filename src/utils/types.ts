377b5a132b4477b692a9c59b020f646bbed4b3a1
Kenneth Yang <microbob@is88.com>
2024-12-19 00:18:21 -0800

    push first pass on schema, update to 0.17.0

---
+++ src/utils/types.ts
@@ -0,0 +1,10 @@
+import type { Tabs } from "wxt/browser";
+
+/**
+ * Add last used attribute to @link Tabs.Tab.
+ *
+ * @param lastUsed - Date object marking when the tab was last used.
+ */
+export interface TimestampedTab extends Tabs.Tab {
+  lastUsed: Date;
+}
